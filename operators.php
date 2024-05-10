9a5fc5d522ca468959bab99068bf9f26f685b95f
sawsuhan <156492698+sawsuhan@users.noreply.github.com>
2024-05-10 22:35:57 +0630

    Add files via upload

--- operators.php
+++ operators.php
@@ -1,4 +1,39 @@
 <?php
- $first=10;
- $second=10;
+ //for Division
+ $y=20;
+ $z=4;
+ echo $y/=$z;
+ echo "<br>";
+
+
+ //for modulus
+ $x=17;
+$h=4;
+ echo $x%=$h;
+ echo "<br>";
+
+ //for Multiplication
+ $s=12;
+$a=5;
+ echo $s*=$a;
+ echo "<br>";
+
+//for less-than equal
+$b=15;
+$c=13;
+if($b<=$c){
+    echo "true";
+}else{
+    echo "false";
+}
+echo "<br>";
+
+//Pre-decrement	
+echo --$b;
+echo "<br>";
+
+//Post-decrement	
+echo $c--;
+
+ ?>
  

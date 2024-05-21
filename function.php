3430941acaa3c6a3686ccd91aa8a9a955411e5f7
sawsuhan <156492698+sawsuhan@users.noreply.github.com>
2024-05-21 14:35:11 +0630

    Add files via upload

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+function greet()
+{
+    echo "hello world"; 
+}
+
+echo greet();
+
+
+ function add($x,$y){
+    $z=$x+$y;
+    echo $z;
+ }
+
+ echo add(20,20);
+ echo "<br>";
+ echo add(30,20);
+?>
